737d2cd3f3fc65601281f9bf154c69ad4a9d6d1d
[b-tom] <tomas.bennasar@gmail.com>
2020-08-26 19:23:18 -0400

    added comment and post models

---
+++ models/Post.model.js
@@ -0,0 +1,16 @@
+const { Schema, model } = require('mongoose');
+
+const postSchema = new Schema(
+  {
+    title: { type: String },
+    content: { type: String },
+    author: { type: Schema.Types.ObjectId, ref:'User'},
+    comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
+    tags: [{ type: String }]
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = model('Post', postSchema)
