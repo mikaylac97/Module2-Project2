7b3d6162babf3565fc5eda40513e236f32fdde47
Mikayla Castro <mikaylacastro97@gmail.com>
2020-09-05 13:37:40 -0400

    mikayla changes

--- routes/collection.routes.js
+++ routes/collection.routes.js
@@ -6,21 +6,19 @@ const Collection = require('../models/Collections.model');
 
 //get route to retrieve the collections page
 router.get('/collections', (req, res, next) =>{
-
-    res.render('collection/collection.hbs');
+  res.render('collection/collection.hbs');
 })
 
+//GET route to retrieve create a collection page
+
+router.get('/collection-create', (req, res, next) =>{
+    res.render('collection/create-collection.hbs');
+  })
 
 //post route to create a collection
 router.post('/collections', (req, res, next) => {
-
     
 })
-
-
-
-
-
 
 
 module.exports = router;
